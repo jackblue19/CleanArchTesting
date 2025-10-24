3423571ded7651d2aef59cf21c12fad1c1d5e289
Jack Blue <git.jack1624@gmail.com>
2025-10-25 06:33:08 +0700

    reset v3

--- BE/CleanArchTesting/Cinema.API/Program.cs
+++ BE/CleanArchTesting/Cinema.API/Program.cs
@@ -1,36 +1,45 @@
+﻿
+using Application.Abstractions.Config;
+using Application.Abstractions.Payments;
+using Application.Abstractions.Persistence;
+using Application.Abstractions.Pricing;
+using Application.Abstractions.Time;
+using Infrastructure.Background;
+using Infrastructure.Data;
+using Infrastructure.Implementations.Payments;
+using Infrastructure.Implementations.Time;
 
-namespace Cinema.API
+namespace Cinema.API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if ( app.Environment.IsDevelopment() )
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if ( app.Environment.IsDevelopment() )
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
--- BE/CleanArchTesting/UnitTests/UnitTest1.cs
+++ BE/CleanArchTesting/UnitTests/UnitTest1.cs
@@ -1,10 +1,10 @@
-//namespace UnitTests;
+namespace UnitTests;
 
-//public class UnitTest1
-//{
-//    [Fact]
-//    public void Test1()
-//    {
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
 
-//    }
-//}
+    }
+}
